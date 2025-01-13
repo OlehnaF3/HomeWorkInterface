8e9e7af06d13d62ec7a13f41cd2221f10ebe0ba6
Oleg <sokol00076@gmail.com>
2025-01-13 22:36:54 +0500

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,69 @@
+﻿using HomeWorkInterface.Intefaces;
+
+namespace HomeWorkInterface
+{
+    class Calculator : ICalculator
+    {
+        ILogger _logger;
+        /// <summary>
+        /// Конструктор с параметрами,
+        /// </summary>
+        /// <param name="logger">Экземпляр класса логгер</param>
+        
+        public Calculator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        int number1, number2;
+        public string[] StringArray { get; private set; } = null;
+
+        /// <summary>
+        /// Старт калькулятора
+        /// </summary>
+        /// <param name="numbers"></param>
+
+        public void Start(string numbers)
+        {
+            while (true)
+                try
+                {
+                    StringArray = numbers.Split(" ");
+                    number1 = Int32.Parse(StringArray[0]);
+                    number2 = Int32.Parse(StringArray[2]);
+                    _logger.Event("Parse");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("{0}, if need help write /help", ex.Message);
+                    _logger.Error("Error");
+                    return;
+                }
+            switch (StringArray[1])
+            {
+                case "+":
+                    Console.WriteLine(Sum(number1, number2));
+                    break;
+                default:
+                    Console.WriteLine("Empty operator");
+                    break;
+            }
+
+        }
+
+        /// <summary>
+        /// Метод суммы 2х чисел!
+        /// </summary>
+        /// <param name="number1"></param>
+        /// <param name="number2"></param>
+        /// <returns>Возвращает целочисленное значение!</returns>
+ 
+        public int Sum(int number1, int number2)
+        {
+            checked
+            {
+                return number1 + number2;
+            }
+        }
+    }
+}
---
+++ Intefaces/ICalculator.cs
@@ -0,0 +1,7 @@
+﻿namespace HomeWorkInterface.Intefaces
+{
+    interface ICalculator
+    {
+        int Sum(int number1, int number2);
+    }
+}
