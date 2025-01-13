3ab62433926151fcdeb5f05a30f5a4a6b1dd3103
Oleg <sokol00076@gmail.com>
2025-01-13 22:36:54 +0500

    Add project files.

---
+++ Logger.cs
@@ -0,0 +1,31 @@
+﻿using HomeWorkInterface.Intefaces;
+namespace HomeWorkInterface
+{
+    public class Logger : ILogger
+    {
+        /// <summary>
+        /// Метод ошибки!
+        /// </summary>
+        /// <param name="message"></param>
+ 
+        public void Error(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+
+        }
+        /// <summary>
+        /// Метод события!
+        /// </summary>
+        /// <param name="message"></param>
+
+        public void Event(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeWorkInterface
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Calculator calc = new Calculator(new Logger());
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (str == "/exit") break;
+                else if (str == "/help")
+                {
+                    Console.WriteLine("Example of how to write \"5\" \"+\" \"5\"");
+                    continue;
+                }
+                calc.Start(str);
+            }
+
+        }
+    }
+
+}
